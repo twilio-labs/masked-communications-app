459a60f3cafb6d90f5d236ec0d071838f6dc1907
Phil <pbrede21@gmail.com>
2022-07-05 16:56:00 -0600

    fixed typo

--- src/twilioClient.ts
+++ src/twilioClient.ts
@@ -9,7 +9,7 @@ if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN)
 
 const client = twilio(
   process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_ACCOUNT_SID
+  process.env.TWILIO_AUTH_TOKEN
 );
 
 export default client;
