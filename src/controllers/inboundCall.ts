3271f679ef5873a0364c775b6021bbb679fd795c
Charlie Weems <charlie.weems@gmail.com>
2022-07-11 14:54:30 -0700

    WIP call handling endpoint

--- src/controllers/inboundCall.ts
+++ src/controllers/inboundCall.ts
@@ -1,8 +1,65 @@
 import type { Request, Response } from "express";
+import { twiml } from 'twilio'
+import { getConversationByAddressPair } from "../utils/getConversationByAddressPair.util";
+const { VoiceResponse } = twiml
+
+import client from '../twilioClient'
+
+const generateTwiml = async (to: string, from: string) => {
+  let response = new VoiceResponse();
+
+  const conversation = await getConversationByAddressPair(from, to)
+
+  if (!conversation) {
+    console.log(`No active session (conversation) for ${from}.`)
+    response.say({
+      voice: process.env.CALL_ANNOUCEMENT_VOICE as any,
+      language: process.env.CALL_ANNOUCEMENT_LANGUAGE as any,
+    }, process.env.OUT_OF_SESSION_MESSAGE_FOR_CALL);
+  } else {
+    const participants = await client.conversations
+      .conversations(conversation.conversationSid)
+      .participants
+      .list()
+
+    const otherParticipants = participants.filter((participant) => {
+      if (participant.messagingBinding.address !== from) {
+        return participant;
+      }
+    })
+
+    response.say({
+      voice: process.env.CALL_ANNOUCEMENT_VOICE as any,
+      language: process.env.CALL_ANNOUCEMENT_LANGUAGE as any
+    }, process.env.CONNECTING_CALL_ANNOUCEMENT);
+
+    if (otherParticipants.length > 1) {
+      // Converence Participants
+    } else {
+      const callee = otherParticipants[0]
+      const dial = response.dial({
+        callerId: callee.messagingBinding.proxy_address
+      });
+
+      dial.number(callee.messagingBinding.address);
+    }
+
+  }
+
+  return response;
+
+}
 
 export const post = async (
-  req: Request<{}, {}, InboundCallBody>,
+  req: Request,
   res: Response
-) => {};
+) => {
+  const { body } = req.body;
+  const { to, from } = body;
 
-interface InboundCallBody {}
+  const twiml = generateTwiml(to, from);
+
+  res.set('Content-Type', 'text/xml')
+  res.send(twiml.toString())
+
+};
---
+++ src/utils/getConversationByAddressPair.util.ts
@@ -0,0 +1,19 @@
+import { ParticipantConversationInstance } from 'twilio/lib/rest/conversations/v1/participantConversation';
+import client from '../twilioClient'
+
+export const getConversationByAddressPair = async (address: string, proxyAddress: string) : Promise<ParticipantConversationInstance> => {
+  if (address === undefined) {
+    throw "getOpenConversationsForAddressPair: address is missing";
+  }
+  const params = {address: address, limit: 500};
+
+  const participantConversations = await client.conversations.participantConversations.list(params);
+  const conversation = participantConversations.find(p => {
+    if (p.conversationState !== 'closed' && p.participantMessagingBinding.proxy_address === proxyAddress) {
+      console.log(`Found a non-closed conversation ${p.conversationSid} with proxy address ${p.participantMessagingBinding.proxy_address} for address ${address}`);
+      return p;
+    }
+  });
+
+  return conversation;
+}
