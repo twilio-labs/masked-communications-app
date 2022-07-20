aaf9c680f137185412e29bcdb8b8ddf4a4c45a41
Charlie Weems <charlie.weems@gmail.com>
2022-07-20 14:09:08 -0700

    Add Twilio webhook validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import * as controllers from "../controllers";
-import basicAuth from 'express-basic-auth'
 
+import twilio from 'twilio'
+import { webhookConfig } from '../config/webhookValidationConfig'
+
+import basicAuth from 'express-basic-auth'
 const { AUTH_USERNAME, AUTH_PASSWORD } = process.env
 
 const router = Router();
 
-router.post("/conversations-post-event", controllers.conversationsPostEvent.post);
+router.post("/conversations-post-event", twilio.webhook(webhookConfig), controllers.conversationsPostEvent.post);
 
-router.post("/inbound-call", controllers.inboundCall.post);
-router.post("/join-conference", controllers.joinConference.get)
+router.post("/inbound-call", twilio.webhook(webhookConfig), controllers.inboundCall.post);
+router.post("/join-conference", twilio.webhook(webhookConfig), controllers.joinConference.get)
 
 router.post("/sessions", basicAuth({
   users: { [AUTH_USERNAME]:AUTH_PASSWORD}
