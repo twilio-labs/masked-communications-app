70059611538c5c46bf7f90a7e89aa3832aadf648
Andrey A <aasfaganov@twilio.com>
2022-07-25 18:44:31 +0200

    wip

--- src/utils/deleteConversation.util.ts
+++ src/utils/deleteConversation.util.ts
@@ -1,20 +1,23 @@
 import client from "../twilioClient"
 
 import retry from 'async-retry'
+import { retryConfig } from "../config/retry.config";
 
-export const deleteConversation = async (conversationSid: string) : Promise<boolean> => {
+export const deleteConversation = async (conversationSid: string, retryOptions = retryConfig) : Promise<boolean> => {
   return retry(async(quit) => {
     try {
       await client.conversations.conversations(conversationSid).remove()
       return true
     } catch (err) {
       if (err.status !== 429) {
-        quit(new Error(err));
+        console.log('Quit without retry')
+        console.log(err)
+        quit(new Error('Quit without retry'));
         return;
       }
 
       console.log('Re-trying on 429 error');
       throw new Error(err);
     } 
-  })
+  }, retryOptions)
 }
