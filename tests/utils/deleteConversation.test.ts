aa347aee58020d2e2b99c4c05c62f937b35ab6b9
Charlie Weems <charlie.weems@gmail.com>
2022-07-29 15:41:06 -0700

    Finish delete tests

--- tests/utils/deleteConversation.test.ts
+++ tests/utils/deleteConversation.test.ts
@@ -33,11 +33,67 @@ describe('deleteConversation util', () => {
     expect(removeSpy).toBeCalled()
   })
 
-  it.skip('throws an error if Twilio client throws', async () => {
+  it('throws an error if Twilio client throws', async () => {
+    let mockedClient = jest.mocked(client, true)
 
+    let removeSpy = jest.fn(() => {
+      throw new Error('Twilio Problem')
+    })
+
+    let conversationsSpy = jest.fn((options) => {
+      return {
+        remove: removeSpy
+      }
+    })
+    
+    mockedClient['conversations'] = {
+      conversations: conversationsSpy
+    } as any
+
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    try {
+      await deleteConversation("myConversationSid");
+    } catch (e) {
+      expect(consoleSpy).toHaveBeenCalledWith('Quit without retry');
+    }
   })
 
-  it.skip('retrys if error is 429', () => {
+  it('retrys if error is 429', async () => {
+    interface TwilioError extends Error {
+      status: number
+    }
 
+    class TwilioError extends Error {
+      constructor(message) {
+        super(message);
+        this.name = "ConcurrencyLimit";
+        this.status = 429
+      }
+    }
+    
+    let mockedClient = jest.mocked(client, true)
+
+    let removeSpy = jest.fn(() => {
+      throw new TwilioError('Too many connections')
+    })
+
+    let conversationsSpy = jest.fn((options) => {
+      return {
+        remove: removeSpy
+      }
+    })
+    
+    mockedClient['conversations'] = {
+      conversations: conversationsSpy
+    } as any
+
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    try {
+      await deleteConversation("myConversationSid",  { retries: 0, factor: 1, maxTimeout: 0, minTimeout: 0 });
+    } catch (e) {
+      expect(consoleSpy).toHaveBeenCalledWith('Re-trying on 429 error');
+    }
   })
 })
