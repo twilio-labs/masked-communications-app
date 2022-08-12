9a48ce89bc5d48dd243103dd76b2f8c88c6d573e
Phil <pbrede21@gmail.com>
2022-08-12 15:41:43 -0600

    added getPhoneNumber function.

--- src/services/phoneNumber.service/index.ts
+++ src/services/phoneNumber.service/index.ts
@@ -1,1 +1,26 @@
+/* eslint-disable space-before-function-paren */
+import areaCodeProximityMap from './area-code-proximity.json'
+import phoneNumberMap from './phone-number-map.json'
 
+export function getPhoneNumber(
+  areaCode: number | string,
+  country: keyof typeof phoneNumberMap,
+  { from = 0, pageSize = 50 }: { from?: number; pageSize?: number } = {
+    from: 0,
+    pageSize: 50
+  }
+) {
+  const optimalPhoneNumbers = []
+
+  let curAreaCode = areaCodeProximityMap[country][areaCode].shift()
+  while (optimalPhoneNumbers.length < from + pageSize && !!curAreaCode) {
+    if (!phoneNumberMap[country][curAreaCode]?.length) {
+      curAreaCode = areaCodeProximityMap[country][areaCode].shift()
+      continue
+    }
+
+    optimalPhoneNumbers.push(phoneNumberMap[country][curAreaCode].shift())
+  }
+
+  return optimalPhoneNumbers.slice(from)
+}
